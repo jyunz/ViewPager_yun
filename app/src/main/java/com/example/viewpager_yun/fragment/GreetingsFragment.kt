ad1efb6b46837370fefdf4ef9f9655def40d50d1
Roy <J.jyun0525@gmail.com>
2022-03-08 11:11:31 +0900

    뷰페이저에 배치될 3개의 프래그먼트 완성

---
+++ app/src/main/java/com/example/viewpager_yun/fragment/GreetingsFragment.kt
@@ -0,0 +1,25 @@
+package com.example.viewpager_yun.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.viewpager_yun.R
+
+class GreetingsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_greetins, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+//        다른 활동이 없기 때문에 비워두었음.
+    }
+}
