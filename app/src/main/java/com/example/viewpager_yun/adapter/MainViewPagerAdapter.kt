e8170b344f77d2fe980d2ffe66bc34ea0b4c0816
Roy <J.jyun0525@gmail.com>
2022-03-08 12:35:09 +0900

    페이지별 제목 설정

--- app/src/main/java/com/example/viewpager_yun/adapter/MainViewPagerAdapter.kt
+++ app/src/main/java/com/example/viewpager_yun/adapter/MainViewPagerAdapter.kt
@@ -25,6 +25,18 @@ class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm){
             return GreetingsFragment()
         }
     }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        if (position == 0) {
+            return "이름"
+        }
+        else if (position == 1) {
+            return "내 정보"
+        }
+        else {
+            return "인사말"
+        }
+    }
 }
 //getItem : 각각의 position에 맞는 Fragment가 어떤 Fragment인지 명시해 주는 함수.
 //getCount : 뷰페이저가 총 몇페이지를 표현하고 싶은지 숫자로 리턴
